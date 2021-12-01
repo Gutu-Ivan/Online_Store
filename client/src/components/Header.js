3f38e8b98512435011deb4c7a929faf8a4eb4d71
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-01 22:36:22 +0200

    Added navbar (Forgot to upload)

---
+++ client/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Header = () => {
+    const showNavigation = () => (
+        <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+            <Link to='#' class='navbar-brand'>
+                Logo
+            </Link>
+            <button
+                class='navbar-toggler'
+                type='button'
+                data-toggle='collapse'
+                data-target='#navbarTogglerDemo02'
+                aria-controls='navbarTogglerDemo02'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+            >
+                <span class='navbar-toggler-icon'></span>
+            </button>
+
+            <div class='collapse navbar-collapse' id='navbarTogglerDemo02'>
+                <ul class='navbar-nav ml-auto mt-2 mt-lg-0'>
+                    <li class='nav-item'>
+                        <Link to='#' class='nav-link'>
+                            Sign Up
+                        </Link>
+                    </li>
+                    <li class='nav-item'>
+                        <Link to='#' class='nav-link'>
+                            Sign In
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+    return <header id='header'>{showNavigation()}</header>;
+};
+
+export default Header;
