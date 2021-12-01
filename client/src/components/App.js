126c5cf8e9a0025ebefc98044a119a12a141b948
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-01 22:22:12 +0200

    Added navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './Header';
+
 
 const App  = () => (
-    <div className="App">
-      <p>App Component</p>
-    </div>
+    <BrowserRouter>
+        <Header/>
+    </BrowserRouter>
+    // <div className="App">
+    //   <p>App Component</p>
+    // </div>
 );
 
 export default App;
