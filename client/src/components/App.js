cb56c2fee7a8c81a1160030497c16030bc6d5ced
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-01 23:02:43 +0200

    Added routes and also created components for them

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
-
+import Home from './Home';
+import Signup from "./Signup";
+import Signin from "./Signin";
+import NotFound from "./NotFound";
 
 const App  = () => (
     <BrowserRouter>
         <Header/>
+        <main>
+            <Routes>
+                <Route exact path={'/'} component={Home} />
+                <Route exact path={'/signup'} component={Signup} />
+                <Route exact path={'/sign'} component={Signin} />
+                <Route component={NotFound} />
+            </Routes>
+        </main>
     </BrowserRouter>
     // <div className="App">
     //   <p>App Component</p>
---
+++ client/src/components/Signup.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Signup = () => {return <p>Sign Up</p>};
+
+export default Signup;
