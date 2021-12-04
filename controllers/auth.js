82b7f1bd9f6a9608ebf71da9c31fda269a8c308e
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-04 23:23:53 +0200

    Added salt for password Hash password Saving user in database Sending success message to signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 exports.signupController = async (req, res) => {
     const {username, email, password1} = req.body;
     try{
@@ -11,9 +12,19 @@ exports.signupController = async (req, res) => {
         const newUser = new User();
         newUser.username = username;
         newUser.email = email;
-        newUser.password1 = password1
+
+        const salt = await bcrypt.genSalt(10);
+        newUser.password1 = await bcrypt.hash(password1,salt);
+
+        await newUser.save();
+        res.json({
+            successMessage: 'Registration success'
+        });
 
     } catch{
         console.log('signupController error: ', err)
+        res.status(500).json({
+            errorMessage: 'Server error',
+        });
     }
 };
