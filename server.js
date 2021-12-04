4cfc8fa9578b18a5d840677b3f968615b2d0b4db
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-04 20:10:04 +0200

    Created backend route for signup component Added proxy to client-package.json

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const db = require('./database/db');
+const authRoutes = require('./routes/auth');
 
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json());
+app.use('/api/auth', authRoutes);
 db();
 
 const port = process.env.PORT || 5000;
